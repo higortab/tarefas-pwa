34c07e645ffb2150697cf78bb23777238321d6d0
higortab <higor.tabraga@gmail.com>
2026-08-09 20:08:04 -0300

    Feat: changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vvkh8n4eads"
+    "revision": "0.011586r17uc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
