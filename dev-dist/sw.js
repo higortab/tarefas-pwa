ebf8fc3c9fc89427b9d4be89c532c699489bd503
higortab <higor.tabraga@gmail.com>
2026-08-09 22:39:13 -0300

    feat: unidade 6

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lqsuaggustk"
+    "revision": "0.89tbqlu9tl4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
